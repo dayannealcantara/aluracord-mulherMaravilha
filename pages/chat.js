5df604c64dc6ced91eab7a61d9635c4e1a94f206
Dayanne Alcantara <dayannealcantara14@gmail.com>
2022-02-02 11:49:28 -0300

    header do chat

--- pages/chat.js
+++ pages/chat.js
@@ -99,6 +99,24 @@ return (
 )
 }
 
+function Header() {
+    return (
+        <>
+         <Box styleSheet={{ width: '100%', marginBottom: '16px', display: 'flex', alignItems: 'center', justifyContent: 'space-between' }} >
+                <Text variant='heading5'>
+                    Chat
+                </Text>
+                <Button
+                    variant='tertiary'
+                    colorVariant='neutral'
+                    label='Logout'
+                    href="/"
+                />
+            </Box>
+        </>
+    )
+}
+
 
 
 )
